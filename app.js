295b7a4806cb44a6f37d57daed393ef7e74ea312
abWasHere <audrey.belson@gmail.com>
2020-10-04 19:25:37 +0200

    starts iteration 4 - get the artist albums

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 const SpotifyWebApi = require("spotify-web-api-node");
 
 /* ---------------------------------------- SETUP EXPRESS */
 const app = express();
-
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
 /* ---------------------------------------- SETUP SPOTIFY API */
-// credentials:
+// Credentials
 const spotifyApi = new SpotifyWebApi({
 	clientId: process.env.CLIENT_ID,
 	clientSecret: process.env.CLIENT_SECRET,
@@ -25,11 +24,41 @@ spotifyApi
 		console.log("Something went wrong when retrieving an access token", error)
 	);
 
-/* ---------------------------------------- SETUP ROUTER */
-app.use("/", require("./routes/index"));
-app.use("/artist-search", require("./routes/index"));
-
-
-app.listen(3000, () =>
-	console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+app.listen(process.env.PORT, () =>
+	console.log(
+		`My Spotify project running on port ${process.env.PORT} 🎧 🥁 🎸 🔊`
+	)
 );
+/* ---------------------------------------- SETUP ROUTER */
+
+// GET homepage
+app.get("/", (req, res) => {
+	res.render("index", {
+		title: "Fake Spotify",
+	});
+});
+
+// GET artists
+app.get("/artist-search", (req, res) => {
+	console.log("trying to get :", req.query);
+
+	spotifyApi
+		.searchArtists(req.query.searchedArtist)
+		.then((data) => {
+			console.log("The received data from the API: ", data.body.artists.items);
+			const artistsRes = data.body.artists.items;
+			res.render("artist-search-results", {
+				title: "Artists found",
+				artistsRes,
+			});
+		})
+		.catch((err) =>
+			console.log("The error while searching artists occurred: ", err)
+		);
+});
+
+// GET artist albums
+app.get("/albums/:artist_id", (req, res, next) => {
+
+
+})
